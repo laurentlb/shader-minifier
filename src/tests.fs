fe54157eddfe2aa1e2c28a01e9aab8fe42c991bd
Eldritch Conundrum <eldritch.conundrum@gmail.com>
2017-04-25 23:49:48 +0200

    fix handling of whitespaces in test filenames

--- src/tests.fs
+++ src/tests.fs
@@ -4,16 +4,21 @@ open System
 open System.IO
 open System.Diagnostics
 
+let binaryFilename = "./shader_minifier.exe"
 let outFilename = Path.Combine(Path.GetTempPath(), "shader_code.h")
 let testFiles = Directory.GetFiles("tests/", "*.*", SearchOption.AllDirectories)
+let quoteArgs = fun s -> "\"" + s + "\""
 
 let () =
+  if not(File.Exists(binaryFilename)) then
+    printfn "You need to compile %s first!" binaryFilename
+    exit 1
   let stopwatch = Stopwatch.StartNew()
   printfn "Running %i tests..." (testFiles.Length)
   let failedTestCount = ref 0
   for filename in testFiles do
-    let p = Process.Start("./shader_minifier.exe",
-                          String.concat " " [ filename; "-o"; outFilename ])
+    let p = Process.Start(binaryFilename,
+                          String.concat " " (List.map quoteArgs [ filename; "-o"; outFilename ]))
     p.WaitForExit()
     if p.ExitCode <> 0 then
       printfn "*** error while testing %s: program returned %i" filename p.ExitCode
