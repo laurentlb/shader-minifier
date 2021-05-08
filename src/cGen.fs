51e0268cf6b294113cd78ec9a40816522760beca
Laurent Le Brun <laurentlb@gmail.com>
2021-05-08 18:29:39 +0200

    cgen: takes an argument for the output stream (#51)

--- src/cGen.fs
+++ src/cGen.fs
@@ -17,12 +17,7 @@ let export ty name (newName:string) =
     else
         exportedValues <- (ty, name, newName) :: exportedValues
 
-let private output() =
-    if Options.debugMode || options.outputName = "" || options.outputName = "-" then stdout
-    else new StreamWriter(options.outputName) :> TextWriter
-
-let private printHeader data asAList =
-    use out = output()
+let private printHeader out data asAList =
     let fileName =
         if options.outputName = "" || options.outputName = "-" then "shader_code.h"
         else Path.GetFileName options.outputName
@@ -55,14 +50,11 @@ let private printHeader data asAList =
 
     if not asAList then fprintfn out "#endif // %s" macroName
 
-let private printNoHeader data =
-    use out = output()
+let private printNoHeader out data =
     let str = [for _, code in data -> Printer.print code] |> String.concat "\n"
     fprintf out "%s" str
 
-let private printJSHeader data =
-    use out = output()
-
+let private printJSHeader out data =
     fprintfn out "/* File generated with Shader Minifier %s" Options.version
     fprintfn out " * http://www.ctrl-alt-test.fr"
     fprintfn out " */"
@@ -79,9 +71,7 @@ let private printJSHeader data =
         fprintfn out "var %s =%s \"%s\"" name Environment.NewLine (Printer.print code)
         fprintfn out ""
 
-let private printNasmHeader data =
-    use out = output()
-
+let private printNasmHeader out data =
     fprintfn out "; File generated with Shader Minifier %s" Options.version
     fprintfn out "; http://www.ctrl-alt-test.fr"
 
@@ -97,9 +87,9 @@ let private printNasmHeader data =
         fprintfn out "_%s:%s\tdb '%s', 0" name Environment.NewLine (Printer.print code)
         fprintfn out ""
 
-let print data = function
-    | Options.Text -> printNoHeader data
-    | Options.CHeader -> printHeader data false
-    | Options.CList -> printHeader data true
-    | Options.JS -> printJSHeader data
-    | Options.Nasm -> printNasmHeader data
+let print out data = function
+    | Options.Text -> printNoHeader out data
+    | Options.CHeader -> printHeader out data false
+    | Options.CList -> printHeader out data true
+    | Options.JS -> printJSHeader out data
+    | Options.Nasm -> printNasmHeader out data
