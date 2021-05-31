d6a91db5dd362f9641fe89e67f53cb5b852533e1
Eldritch Conundrum <eldritch.conundrum@gmail.com>
2021-05-31 23:14:24 +0200

    Remove old files

--- compile.bash
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-references='-r lib/FParsec.dll -r lib/FParsecCS.dll -r lib/FSharp.PowerPack.dll'
-fsharpc --standalone $references src/{ast.fs,printer.fs,cGen.fs,renamer.fs,rewriter.fs,parse.fs,main.fs} -o shader_minifier.exe
