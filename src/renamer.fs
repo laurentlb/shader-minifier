95e53f2fef4a8f3433961d147879b42fe571897c
Laurent Le Brun <laurentlb@gmail.com>
2021-06-05 01:46:00 +0200

    delete unused line

--- src/renamer.fs
+++ src/renamer.fs
@@ -350,7 +350,6 @@ module private RenamerImpl =
         // Rename local variables.
         for shader in shaders do
             shader.code <- List.map (renTopLevelBody env) shader.code
-            exportedNames <- !env.exportedNames
 
         exportedNames
 
