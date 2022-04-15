860ee1da6029a5b69935f6a20de3fd649e41dea9
Knut Arild Erstad <knutae@gmail.com>
2022-04-15 23:26:08 +0200

    Fix outdated compile.bash script

--- compile.bash
+++ compile.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-references='-r lib/FParsec.dll -r lib/FParsecCS.dll -r lib/FSharp.PowerPack.dll'
-fsharpc --standalone $references src/{ast.fs,printer.fs,cGen.fs,renamer.fs,rewriter.fs,parse.fs,main.fs} -o shader_minifier.exe
+references='-r lib/Argu.dll -r lib/FParsec.dll -r lib/FParsecCS.dll'
+fsharpc --standalone $references src/{options.fs,ast.fs,printer.fs,formatter.fs,renamer.fs,rewriter.fs,parse.fs,main.fs} -o shader_minifier.exe
