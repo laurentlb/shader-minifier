decb7891382257762505dd8b5fa5f46a69e70955
Laurent Le Brun <laurentlb@gmail.com>
2022-10-11 22:19:33 +0200

    Remove the mandatory dependency on Argu
    I didn't manage to get Argu to work on Wasm. This refactoring removes the references to Argu in the Options object.

--- src/options.fs
+++ src/options.fs
@@ -55,72 +55,60 @@ type CliArguments =
             | Smoothstep -> "Use IQ's smoothstep trick"
             | Filenames _ -> "List of files to minify" 
 
-
-let argParser = ArgumentParser.Create<CliArguments>(helpTextMessage =
-    sprintf "Shader Minifier %s - https://github.com/laurentlb/Shader_Minifier" version)
-
-type Options() =
-    let mutable args: ParseResults<CliArguments> =
-        Unchecked.defaultof<ParseResults<CliArguments>>
-
-    member this.outputName =
-        args.GetResult(OutputName, defaultValue = "shader_code.h")
-
-    member this.outputFormat =
-        args.GetResult(FormatArg, defaultValue = CHeader)
-
-    member this.verbose =
-        args.Contains(Verbose)
-
-    member this.smoothstepTrick =
-        args.Contains(Smoothstep)
-
-    member this.canonicalFieldNames =
-        (sprintf "%A" (args.GetResult(FieldNames, defaultValue = XYZW))).ToLower()
-
-    member this.preserveExternals =
-        args.Contains(PreserveExternals) || args.Contains(PreserveAllGlobals)
-    
-    member this.preserveAllGlobals =
-        args.Contains(PreserveAllGlobals)
-    
-    member val reorderDeclarations = false with get, set
-    member val reorderFunctions = false with get, set
-
-    member this.hlsl =
-        args.Contains(Hlsl)
-
-    member this.noInlining =
-        args.Contains(NoInlining)
-
-    member this.aggroInlining =
-        args.Contains(AggroInlining) && not this.noInlining
-
-    member this.noSequence =
-        args.Contains(NoSequence)
-
-    member this.noRenaming =
-        args.Contains(NoRenaming)
-
-    member val noRenamingList = ["main"] with get, set
-
-    member this.filenames =
-        args.GetResult(Filenames, defaultValue=[]) |> List.toArray
-
-    member this.init(argv) =
-        args <- argParser.Parse(argv)
-        
-        let opt = args.GetResult(NoRenamingList, defaultValue = "main")
-        this.noRenamingList <- [for i in opt.Split([|','|]) -> i.Trim()]
-
-        if this.filenames.Length = 0 then
-            printfn "%s" (argParser.PrintUsage(message = "Missing parameter: the list of shaders to minify"))
-            false
-        else
-            true
-
 module Globals =
-    let options = Options()
+    let mutable options = {|
+        outputName = "shader_code.h"
+        outputFormat = CHeader
+        verbose = false
+        smoothstepTrick = false
+        canonicalFieldNames = "xyzw"
+        preserveExternals = false
+        preserveAllGlobals = false
+        reorderDeclarations = ref false
+        reorderFunctions = ref false
+        hlsl = false
+        noInlining = false
+        aggroInlining = false
+        noSequence = false
+        noRenaming = false
+        noRenamingList = ["main"]
+        filenames = ([||]: string[])
+    |}
 
     // like printfn when verbose option is set
     let vprintf fmt = fprintf (if options.verbose then stdout else TextWriter.Null) fmt
+
+let init(argv) =
+    let argParser = ArgumentParser.Create<CliArguments>(
+        helpTextMessage =
+            sprintf "Shader Minifier %s - https://github.com/laurentlb/Shader_Minifier" version)
+
+    let args = argParser.Parse(argv)
+
+    let opt = args.GetResult(NoRenamingList, defaultValue = "main")
+    let noRenamingList = [for i in opt.Split([|','|]) -> i.Trim()]
+    let filenames = args.GetResult(Filenames, defaultValue=[]) |> List.toArray
+
+    if filenames.Length = 0 then
+        printfn "%s" (argParser.PrintUsage(message = "Missing parameter: the list of shaders to minify"))
+        false
+    else
+        Globals.options <- {|
+            outputName = args.GetResult(OutputName, defaultValue = "shader_code.h")
+            outputFormat = args.GetResult(FormatArg, defaultValue = CHeader)
+            verbose = args.Contains(Verbose)
+            smoothstepTrick = args.Contains(Smoothstep)
+            canonicalFieldNames = (sprintf "%A" (args.GetResult(FieldNames, defaultValue = XYZW))).ToLower()
+            preserveExternals = args.Contains(PreserveExternals) || args.Contains(PreserveAllGlobals)
+            preserveAllGlobals = args.Contains(PreserveAllGlobals)
+            reorderDeclarations = ref false
+            reorderFunctions = ref false
+            hlsl = args.Contains(Hlsl)
+            noInlining = args.Contains(NoInlining)
+            aggroInlining = args.Contains(AggroInlining) && not (args.Contains(NoInlining))
+            noSequence = args.Contains(NoSequence)
+            noRenaming = args.Contains(NoRenaming)
+            noRenamingList = noRenamingList
+            filenames = filenames
+        |}
+        true
