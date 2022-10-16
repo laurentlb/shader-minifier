213eae5357ab529423f7f97a9b8900e8cf55569e
Laurent Le Brun <laurentlb@gmail.com>
2022-10-16 22:20:15 +0200

    Split the project in two projects: a library and the binary
    This lets us reuse the Shader Minifier library more easily. The first motivation is for the WebAssembly build. Other people might also want to use the library from the .NET tool.
    
    As a side bonus, the Checker can now be used in Release mode (the --standalone flag on the executable was breaking this).

---
+++ src/api.fs
@@ -0,0 +1,54 @@
+﻿module ShaderMinifier
+
+open System
+open System.IO
+open Microsoft.FSharp.Text
+open Options.Globals
+
+let printSize (shaders: Ast.Shader[]) =
+    if options.verbose then
+        let length = shaders |> Array.map (fun s -> Printer.printText s.code)
+                   |> Array.sumBy (fun s -> s.Length)
+        printfn "Shader size is: %d" length
+
+let readFile file =
+    let stream =
+        if file = "" then new StreamReader(Console.OpenStandardInput())
+        else new StreamReader(file)
+    stream.ReadToEnd()
+
+let minify (files: (string*string)[]) =
+    vprintf "Input file size is: %d\n" (files |> Array.sumBy (fun (_, s) -> s.Length))
+    let shaders = files |> Array.map (fun (f, c) -> Parse.runParser f c)
+    vprintf "File parsed. "; printSize shaders
+
+    for shader in shaders do
+        shader.code <- Rewriter.reorder shader.code
+        shader.code <- Rewriter.simplify shader.code
+    vprintf "Rewrite tricks applied. "; printSize shaders
+
+    if options.noRenaming then
+        shaders, []
+    else
+        let exportedNames = Renamer.rename shaders
+        vprintf "Identifiers renamed. "; printSize shaders
+        shaders, exportedNames
+
+let minifyFiles files =
+    let files = files |> Array.map (fun f ->
+        let content = readFile f
+        let filename = if f = "" then "stdin" else f
+        filename, content)
+    minify files
+
+let run files =
+    use out =
+        if Options.debugMode || options.outputName = "" || options.outputName = "-" then stdout
+        else new StreamWriter(options.outputName) :> TextWriter
+    try
+        let shaders, exportedNames = minifyFiles files
+        Formatter.print out shaders exportedNames options.outputFormat
+        0
+    with exn ->
+        printfn "%s" (exn.ToString())
+        1
--- src/main.fs
+++ src/main.fs
@@ -1,57 +1,6 @@
 ﻿module Main
 
-open System
-open System.IO
-open Microsoft.FSharp.Text
 open Options.Globals
-
-let printSize (shaders: Ast.Shader[]) =
-    if options.verbose then
-        let length = shaders |> Array.map (fun s -> Printer.printText s.code)
-                   |> Array.sumBy (fun s -> s.Length)
-        printfn "Shader size is: %d" length
-
-let readFile file =
-    let stream =
-        if file = "" then new StreamReader(Console.OpenStandardInput())
-        else new StreamReader(file)
-    stream.ReadToEnd()
-
-let minify (files: (string*string)[]) =
-    vprintf "Input file size is: %d\n" (files |> Array.sumBy (fun (_, s) -> s.Length))
-    let shaders = files |> Array.map (fun (f, c) -> Parse.runParser f c)
-    vprintf "File parsed. "; printSize shaders
-
-    for shader in shaders do
-        shader.code <- Rewriter.reorder shader.code
-        shader.code <- Rewriter.simplify shader.code
-    vprintf "Rewrite tricks applied. "; printSize shaders
-
-    if options.noRenaming then
-        shaders, []
-    else
-        let exportedNames = Renamer.rename shaders
-        vprintf "Identifiers renamed. "; printSize shaders
-        shaders, exportedNames
-
-let minifyFiles files =
-    let files = files |> Array.map (fun f ->
-        let content = readFile f
-        let filename = if f = "" then "stdin" else f
-        filename, content)
-    minify files
-
-let run files =
-    use out =
-        if Options.debugMode || options.outputName = "" || options.outputName = "-" then stdout
-        else new StreamWriter(options.outputName) :> TextWriter
-    try
-        let shaders, exportedNames = minifyFiles files
-        Formatter.print out shaders exportedNames options.outputFormat
-        0
-    with exn ->
-        printfn "%s" (exn.ToString())
-        1
 
 [<EntryPoint>]
 let main argv =
@@ -60,7 +9,7 @@ let main argv =
             if Options.init argv then 
                 if options.verbose then
                     printfn "Shader Minifier %s - https://github.com/laurentlb/Shader_Minifier" Options.version
-                run options.filenames
+                ShaderMinifier.run options.filenames
             else 1
         with
         | :? Argu.ArguParseException as ex ->
