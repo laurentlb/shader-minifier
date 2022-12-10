2ee0a8f4451a42fe7b9c2a852cdd15c90c3a3f71
Laurent Le Brun <laurentlb@gmail.com>
2022-12-10 20:07:51 +0100

    Inlining: inline more variables
    If a variable is used once, and its init value depends on values proven constant, we can inline it.
    
    e.g.
      float a= f();
      float b = a + 1;
      float c = b;
    
    now becomes:
      float a= f();
      float c = a + 1;

--- src/ast.fs
+++ src/ast.fs
@@ -4,16 +4,13 @@ open Options.Globals
 
 type Ident(name: string) =
     let mutable newName = name
-    let mutable inlined = newName.StartsWith("i_")
-    let mutable lValue = false
 
     member this.Name = newName
     member this.OldName = name
     member this.Rename(n) = newName <- n
-    member this.ToBeInlined = inlined
-    member this.Inline() = inlined <- true
-    member this.IsLValue = lValue
-    member this.MarkLValue() = lValue <- true
+    member val ToBeInlined = newName.StartsWith("i_") with get, set
+    member val IsLValue = false with get, set
+    member val IsConst = false with get, set
 
      // Real identifiers cannot start with a digit, but the temporary ids of the rename pass are numbers.
     member this.IsUniqueId = System.Char.IsDigit this.Name.[0]
