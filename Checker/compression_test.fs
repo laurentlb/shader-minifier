8f7d97d3f86a0b0692e7946dcbdfda0bb6ed2779
Laurent Le Brun <laurentlb@gmail.com>
2023-01-16 00:28:11 +0100

    Add test for multifile minification & compression

--- Checker/compression_test.fs
+++ Checker/compression_test.fs
@@ -47,29 +47,40 @@ let log fmt =
 
     Printf.ksprintf logger fmt
 
-let testFile (file: string) =
-    let langArg = if file.EndsWith("hlsl") then [|"--hlsl"|] else [||]
-    let extraArgs = [|"--format"; "text"|]
-    Options.init(Array.append langArg extraArgs)
+let compressionTest args files =
+    Options.init(args)
     let minified =
         use out = new StringWriter()
-        let shaders, exportedNames = ShaderMinifier.minifyFiles [|"tests/real/" + file|]
+        let shaders, exportedNames = ShaderMinifier.minifyFiles [|for f in files -> "tests/real/" + f|]
         Formatter.print out shaders exportedNames Options.Text
         out.ToString().ToCharArray()
 
     let pointer = &&minified.[0]
-    log "%-40s " file
+    log "%-40s " (match files with [f] -> f | f::_ -> f + "...")
     log "%5d " minified.Length
     let compressedSize = Crinkler.ApproximateModels4k(pointer, minified.Length)
     log "=> %8.3f\n" compressedSize
     minified.Length, float compressedSize
 
+let compressFile (file: string) =
+    let langArg = if file.EndsWith("hlsl") then [|"--hlsl"|] else [||]
+    let extraArgs = [|"--format"; "text"|]
+    compressionTest (Array.append langArg extraArgs) [file]
+
 let run () =
     Crinkler.InitCompressor() // Platform must be set to x64
 
     writer.GetStringBuilder().Clear() |> ignore<StringBuilder>
-    let sizes = List.map testFile testFiles
+
+    // Tests for minifying multiple files together.
+    // We don't have good examples of files that fit together, but these files use the same uniforms (e.g. time, tex0).
+    let multifileInputs = ["clod.frag"; "leizex.frag"; "slisesix.frag"; "motion_blur.frag"; "mandel.frag"; "kaleidoscope.frag"]
+    let multifileOutput = compressionTest [|"--format"; "text"|] multifileInputs
+
+    // Tests for individual files.
+    let sizes = multifileOutput :: List.map compressFile testFiles
     let minifiedSum = List.sumBy fst sizes
     let compressedSum = List.sumBy snd sizes
     log "Total: %5d => %9.3f\n" minifiedSum compressedSum
+
     File.WriteAllText("tests/compression_results.log", writer.ToString())
