a868eb4445018e8c1c52e6ce1bd2db9c1d46d940
Eldritch Conundrum <eldritch.conundrum@gmail.com>
2023-03-24 18:34:18 -0700

    Detect more pure builtin functions (#267)

--- src/analyzer.fs
+++ src/analyzer.fs
@@ -34,8 +34,21 @@ let mathsFunctions = set([
 let vectorFunctions = set([
     "cross"; "distance"; "dot"; "equal"; "faceforward"; "length"; "normalize";
     "notEqual"; "reflect"; "refract"])
+let builtinTypes = set([
+    yield! [ "void"; "bool"; "int"; "uint"; "float"; "double" ]
+    for p in ["d"; "b"; "i"; "u"] do
+        for n in ["2"; "3"; "4"] do
+            yield p+"vec"+n
+    for p in [""; "d"] do
+        for n in ["2"; "3"; "4"] do
+            yield p+"mat"+n
+        for c in ["2"; "3"; "4"] do
+            for r in ["2"; "3"; "4"] do
+                yield p+"mat"+c+"x"+r
+    ])
+let castFunctions = builtinTypes - set ["void"]
 
-let pureBuiltinFunctions = trigonometryFunctions + mathsFunctions + vectorFunctions
+let pureBuiltinFunctions = trigonometryFunctions + mathsFunctions + vectorFunctions + castFunctions
 
 // Mark variables as inlinable when possible.
 // Variables are always safe to inline when all of:
