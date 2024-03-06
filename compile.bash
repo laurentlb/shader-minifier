2e3a9a70c617baa8c403c23b2ad4f7301cff5b60
Eldritch Conundrum <eldritch.conundrum@gmail.com>
2024-03-07 00:51:49 +0100

    fix compilation on linux

--- compile.bash
+++ compile.bash
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-references='-r lib/Argu.dll -r lib/FParsec.dll -r lib/FParsecCS.dll'
-fsharpc --standalone $references src/{options.fs,ast.fs,printer.fs,formatter.fs,renamer.fs,rewriter.fs,parse.fs,main.fs} -o shader_minifier.exe
+dotnet build shader-minifier-linux.fsproj
