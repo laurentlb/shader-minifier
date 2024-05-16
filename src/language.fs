90ad2cd7f3d7ebc322179f96b0f6fb73cfddb2d9
Theron Tarigo <theron.tarigo@gmail.com>
2024-05-16 17:31:22 -0400

    Cleanup

--- src/language.fs
+++ src/language.fs
@@ -2,16 +2,14 @@
 
 type Language() =
     member val hlsl = false with get, set
-    member val glslver = 0 with get, set
-    member val esslver = 0 with get, set
-    member val dynamicloop = false with get, set
+    member val dynamicloop = true with get, set
     member val implicitconv = false with get, set
 
 module Globals =
     let mutable language = Language()
-    let LanguageHLSL ver =
-      Language (hlsl = true, dynamicloop = true)
+    let LanguageHLSL _ =
+        Language (hlsl = true)
     let LanguageGLSL ver =
-      Language (glslver = ver, dynamicloop = true, implicitconv = true)
+        Language (implicitconv = true)
     let LanguageESSL ver =
-      Language (esslver = ver, dynamicloop = (ver>=300))
+        Language (dynamicloop = (ver>=300))
